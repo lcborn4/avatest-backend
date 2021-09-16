6c4189dbf1cc2e7b9a86d7551b942b7bbe819192
Logan <lcborn4@gmail.com>
2021-09-16 16:32:53 -0400

    basic routes added

--- index.js
+++ index.js
@@ -6,6 +6,35 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/ping', (req, res) => {
+
+  let message = {
+    "ok": true,
+    "msg": "pong"
+  };
+
+  res.status(200).send(message);
+})
+
+app.get('/info', (req,res)=>{
+
+  //need to fill
+  let message = 
+  {
+    "ok": true,
+    "author": {
+      "email": "lcborn4@gmail.com",
+      "name": "Logan Born"
+    },
+    "frontend": {
+      "url": "string, the url of your frontend."
+    },
+    "language": "node.js",
+    "sources": "string, the url of a github repository including your backend sources and your frontend sources"
+  }
+  res.status(200).send(message);
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
